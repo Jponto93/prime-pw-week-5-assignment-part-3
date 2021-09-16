9e956f376b5d4c97a364c369cfe3f1d30a711029
Jordan Ponto <jponto93@gmail.com>
2021-09-16 09:16:31 -0500

    add to collection function created

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,21 @@
 console.log('***** Music Collection *****')
+
+const collection = [];
+
+const addToCollection = (title, artist, yearPublished) => {
+  console.log('in addToCollection function');
+  album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }//end inputs
+  collection.push(album) //adding item to end of collection
+} //end addToCollection
+
+addToCollection('Daisy World','Tyler The Creator',2021);
+addToCollection('Man On The Moon','Kid Cudi',2009);
+addToCollection('So Far Gone','Drake',2009);
+addToCollection('Man On The Moon III','Kid Cudi',2020);
+addToCollection('Walking On A Dream','Empire of the Sun',2009);
+addToCollection('El Gato: The Human Glacier','Gucci Mane',2017);
+console.log(collection);
